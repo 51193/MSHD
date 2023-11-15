cf7948982f5b33e8e6ed1009d10fa825a89eeb36
2779606141 <2779606141@qq.com>
2023-11-15 18:31:24 +0800

    新增了菜单栏

--- src/app/layout/services/menus.service.ts
+++ src/app/layout/services/menus.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Menus } from '../interfaces/menu';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {Menus} from '../interfaces/menu';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MenusService {
-  private _menus$ = new BehaviorSubject<Menus>([]);
+  private _menus$ = new BehaviorSubject<Menus>([{
+    level: 1, title: "首页", icon: "dashboard", selected: true, disabled: false,
+  }]);
   get menus$(): BehaviorSubject<Menus> {
     return this._menus$;
   }
+}
+
+export interface Menu {
+  level: number;
+  title: string;
+  icon: string | null;
+  selected: boolean;
+  disabled: boolean;
+  routerLink?: string[];
+  open?: boolean;
+  children?: Menu[];
 }
