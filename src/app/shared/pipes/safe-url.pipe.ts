d904885b60f7389b3d58a530fc360d0703edcadb
crepusculumx <crepusculumx@qq.com>
2023-12-20 17:32:52 +0800

    map

--- src/app/shared/pipes/safe-url.pipe.ts
+++ src/app/shared/pipes/safe-url.pipe.ts
@@ -1,6 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { environment } from '../../../environments/environment';
 
 @Pipe({
   name: 'safeUrl',
@@ -9,9 +8,9 @@ export class SafeUrlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
   transform(url: string): SafeResourceUrl {
-    if (!url.startsWith(environment.api.baseUrl)) {
-      return this.sanitizer.bypassSecurityTrustResourceUrl('');
-    }
+    // if (!url.startsWith(environment.api.baseUrl)) {
+    //   return this.sanitizer.bypassSecurityTrustResourceUrl('');
+    // }
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
